64b590d3637f166aa67243e280651975ef818326
alessandro stoppato <alexrah@gmail.com>
2024-09-03 16:29:57 +0200

    test/useId.test.tsx: add test for custom prefixes

--- test/useId.test.tsx
+++ test/useId.test.tsx
@@ -2,33 +2,36 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import {useId, IdProvider, useGetId} from '../src'
 
+const MultiId: React.FunctionComponent = ({children}) => {
+	const getId = useGetId()
+	return (
+		<>
+			{getId()} + {getId()}
+			{children && (
+				<>
+					: {children}
+					: <Id />
+					: <MultiId />
+				</>
+			)}
+		</>
+	)
+}
+
+const Id: React.FunctionComponent = ({children}) => {
+	const id = useId()
+	return (
+		<>
+			{id}
+			{children && <>:{children}</>}
+		</>
+	)
+}
+
 describe('it', () => {
-	it('creates ids', () => {
+	it('creates ids with standard prefix', () => {
 		const div = document.createElement('div')
-		const Id: React.FunctionComponent = ({children}) => {
-			const id = useId()
-			return (
-				<>
-					{id}
-					{children && <>:{children}</>}
-				</>
-			)
-		}
-		const MultiId: React.FunctionComponent = ({children}) => {
-			const getId = useGetId()
-			return (
-				<>
-					{getId()} + {getId()}
-					{children && (
-						<>
-							: {children}
-							: <Id />
-							: <MultiId />
-						</>
-					)}
-				</>
-			)
-		}
+
 		const App = () => (
 			<IdProvider>
 				<Id />
@@ -41,6 +44,24 @@ describe('it', () => {
 		ReactDOM.render(<App />, div)
 		expect(div.innerHTML).toBe(
 			`id-0 / id-1:id-2 - id-3-0 + id-3-1: Hi: id-4: id-5-0 + id-5-1`
+		)
+		ReactDOM.unmountComponentAtNode(div)
+	})
+
+	it('works with different prefixes', () => {
+		const div = document.createElement('div')
+		const App = () => (
+			<IdProvider prefix="my">
+				<Id />
+				{' / '}
+				<Id>
+					<Id /> - <MultiId>Hi</MultiId>
+				</Id>
+			</IdProvider>
+		)
+		ReactDOM.render(<App />, div)
+		expect(div.innerHTML).toBe(
+			`my-0 / my-1:my-2 - my-3-0 + my-3-1: Hi: my-4: my-5-0 + my-5-1`
 		)
 		ReactDOM.unmountComponentAtNode(div)
 	})
